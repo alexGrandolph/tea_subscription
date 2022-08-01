ff8c77c07db348658a9bb7c82b03df4ce54f7c89
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-08-01 14:44:51 -0600

    feat: basic customer validations passing

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Customer type: :model do
+RSpec.describe Customer, type: :model do
   describe "Validations" do
 
-    it { should_validate_presence_of :first_name }
-    it { should_validate_presence_of :last_name }
-    it { should_validate_presence_of :email }
-    it { should_validate_presence_of :address }
+    it { should validate_presence_of(:first_name )}
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:address) }
     it { should validate_uniqueness_of(:email) }
 
 
