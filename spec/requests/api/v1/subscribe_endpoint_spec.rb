b10fc66a31542d5cf0bc77840efd44aa2f204ad5
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-08-02 14:25:48 -0600

    Test: add intial test for subscribe user endpoint

---
+++ spec/requests/api/v1/subscribe_endpoint_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'Subscribe User Endpoint' do
+  describe 'Happy Path' do
+    
+    it 'Allows a user to subscribe to a Tea Subscription' do
+      user1 = User.create!(first_name: 'John', last_name: 'Brisket', email: 'john@example.com', address: '123 Fake Street')
+      user1 = User.create!(first_name: 'Bobby', last_name: 'Pork Belly', email: 'bobbyG@example.com', address: '1556 North South St.')
+
+      tea1 = Tea.create!(title: 'Earl Grey', description: 'A tasty tea for sure', temp: 55, brewtime: '5')
+      tea2 = Tea.create!(title: 'Green Tea', description: 'A tea that is green', temp: 62, brewtime: '2')
+      headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+      body = {
+        user_id: user1.id,
+        tea_id: tea1.id,
+        title: 'Monthly Early Grey',
+        price: 45.50,
+        frequency: 2
+      }
+      
+      get '/api/v1/subscribe', headers: headers, params: body
+      
+      response = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+      expect(response).to have key(:data)
+      expect(response[:data]).to be_a Hash
+      data = response[:data]
+
+    end
+
+
+
+
+
+  end 
+
+end
