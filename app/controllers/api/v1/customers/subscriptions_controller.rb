7a0ada86356424772945ccb68254e8a89a28fa40
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-08-03 18:19:40 -0600

    feat: add seeds for presentation

--- app/controllers/api/v1/customers/subscriptions_controller.rb
+++ app/controllers/api/v1/customers/subscriptions_controller.rb
@@ -3,8 +3,18 @@ class Api::V1::Customers::SubscriptionsController < ApplicationController
   def index
     customer = Customer.find(params[:customer_id])
     render json: CustomerSerializer.new(customer)
-    # binding.pry
   end 
 
+
+  # def update
+  #   if params[:cancel] == 'true'
+  #     subscription = Subscription.find(params[:subscription_id])
+  #     subscription.update(status: 1)
+  #     subscription.save
+  #     render json: SubscriptionSerializer.new(subscription)
+  #   else
+  #   end
+
+  # end 
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Customer.destroy_all
+Tea.destroy_all
+Subscription.destroy_all
+
+customer1 = Customer.create!(first_name: 'John', last_name: 'Brisket', email: 'john@example.com', address: '123 Fake Street')
+customer2 = Customer.create!(first_name: 'Bobby', last_name: 'Pork Belly', email: 'bobbyG@example.com', address: '1556 North South St.')
+
+tea1 = Tea.create!(title: 'Earl Grey', description: 'A tasty tea for sure', temp: 55, brewtime: '5')
+tea2 = Tea.create!(title: 'Green Tea', description: 'A Tea that is green', temp: 40, brewtime: '2')
+tea3 = Tea.create!(title: 'Hibiscus', description: 'probably the best tea', temp: 88, brewtime: '7')
+
+ubscription1 = Subscription.create!(title: 'Monthly Early Grey', price: 49.98, status: 0, frequency: 1, customer_id: customer1.id, tea_id: tea1.id)
+subscription2 = Subscription.create!(title: 'Monthly Green Tea', price: 49.98, status: 1, frequency: 1, customer_id: customer1.id, tea_id: tea2.id)
+subscription3 = Subscription.create!(title: 'Weekly Hibiscus', price: 49.98, status: 1, frequency: 0, customer_id: customer1.id, tea_id: tea3.id)
