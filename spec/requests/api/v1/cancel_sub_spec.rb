20ddcfa3e2420ec648709cd236ebb47e1a049b02
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-08-04 11:14:51 -0600

    Test: add test for not including / cancel false in cancel sub request

--- spec/requests/api/v1/cancel_sub_spec.rb
+++ spec/requests/api/v1/cancel_sub_spec.rb
@@ -54,6 +54,29 @@ RSpec.describe 'Cancel Customer Subscription Endpoint' do
 
     end 
 
+    it 'Not including cancel: true returns an error' do
+      customer1 = Customer.create!(first_name: 'John', last_name: 'Brisket', email: 'john@example.com', address: '123 Fake Street')
+      tea1 = Tea.create!(title: 'Some Tea', description: 'A tasty tea for sure', temp: 55, brewtime: '5')
+      subscription = Subscription.create!(title: 'Weekly Tea', price: 49.98, status: 0, frequency: 0, customer_id: customer1.id, tea_id: tea1.id)
+
+      post '/api/v1/subscriptions/cancel', headers: headers, params:{ subscription_id: subscription.id, cancel: false }
+
+      expect(response).to be_successful
+
+      result = JSON.parse(response.body, symbolize_names: true)
+      expect(result).to have_key(:data)
+      expect(result[:data]).to be_a Hash
+      
+      data = result[:data]
+      expect(data).to have_key(:type)
+      expect(data[:type]).to eq("Error")
+
+      expect(result[:data]).to have_key(:attributes)
+      expect(result[:data][:attributes]).to have_key(:message)
+      expect(result[:data][:attributes][:message]).to eq("Please include cancel: true to parameters to cancel this subscription")
+
+    end 
+
 
 
 
